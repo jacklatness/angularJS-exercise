d3d97b528b6b62c97175787da60fbbd6dfb11975
John Inocente <jccinocente@Oslik-Pro.local>
2022-06-28 18:50:47 +0800

    Describe your commit here

--- app/core/version/canada-phone-directive.js
+++ app/core/version/canada-phone-directive.js
@@ -15,9 +15,6 @@ angular.module('myApp.version.canada-phone-directive', [])
                 //into an array using the indexes as a regular expression 
                 var x = numVal.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
                 e.target.value = !x[2] ? x[1] : '(' + x[1] + ')' + x[2] + (x[3] ? '-' + x[3] : '');
-        
-                scope.ngModel = numVal; // overwrites ngModel value
-                console.log(scope.ngModel);
             });
         }
     };
